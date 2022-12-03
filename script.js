26cc6290a5c88d0a9da89f518479f67a4bb2b7be
Darwin Valdez <87610626+DarwinValdez@users.noreply.github.com>
2022-12-03 14:25:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+const bottonNumeros = document.getElementsByName("date-number");
+const bottonOpera = document.getElementsByName("date-opera");
+const bottonIgual = document.getElementsByName("date-igual")[0];
+const bottonDelete = document.getElementsByName("date-delete");
+let result = document.getElementById("result");
+let opeActual = '';
+let opeAnterior = '';
+let operacion = undefined;
+
+bottonNumeros.forEach(function (botton) {
+  botton.addEventListener("click", function () {
+    agregarNumero(botton.innerText);
+  });
+});
+
+bottonOpera.forEach(function (botton) {
+  botton.addEventListener("click", function () {
+    selectOperacion(botton.innerText);
+  });
+});
+
+bottonIgual.addEventListener("click", function () {
+  calcular();
+  actualizarDisplay();
+});
+
+bottonDelete.addEventListener("click", function () {
+  clear();
+  actualizarDisplay();
+});
+
+function selectOperacion(op) {
+  if (opeActual === '') return;
+  if (opeAnterior !== '') {
+    calcular();
+  }
+  operacion = op.toString();
+  opeAnterior = opeActual;
+  opeActual = '';
+}
+
+function calcular() {
+  let calculo;
+  const anterior = parseFloat(opeAnterior);
+  const actual = parseFloat(opeActual);
+
+  if (isNaN(anterior) || isNaN(actual)) result;
+  switch (operacion) {
+    case "+":
+      calculo = anterior + actual;
+      break;
+    case "-":
+      calculo = anterior - actual;
+      break;
+    case "x":
+      calculo = anterior * actual;
+      break;
+    case "/":
+      calculo = anterior / actual;
+
+      default:
+        return;
+  }
+
+  opeActual= calculo
+  operacion= undefined
+  opeAnterior='';
+}
+
+function agregarNumero(num) {
+  opeActual = opeActual.toString() + num.toString();
+  actualizarDisplay();
+}
+
+function clear() {
+  opeActual = "";
+  opeAnterior = "";
+  operacion = undefined;
+}
+
+function actualizarDisplay() {
+  result.value = opeActual;
+}
